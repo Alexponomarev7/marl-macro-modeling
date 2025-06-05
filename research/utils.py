962a09fc26b984e88290a7c0a0d4aace700275c9
Alex Ponomarev <alex.ponomarev@pinely.com>
2025-06-05 23:43:30 +0200

    Add test and fix Capital access for Env (#15)
    Add tests for validation dynare and gymnasium
    Fix capital access in Ramsey

--- research/utils.py
+++ research/utils.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
-ROOT = Path(__file__).parent.parent
-
 class PathStorage:
     def __init__(self, data_folder: str = "data"):
-        self.raw_root = ROOT / data_folder / "raw"
-        self.processed_root = ROOT / data_folder / "processed"
-        self.dynare_configs_root = ROOT / "dynare/docker/dynare_models"
+        self.root = Path(__file__).parent.parent
+        self.raw_root = self.root / data_folder / "raw"
+        self.processed_root = self.root / data_folder / "processed"
+        self.dynare_configs_root = self.root / "dynare/docker/dynare_models"
